9bd1a91071112b47fc491bc53d861b9168f92de3
Akash <AwesomeAkash47@gmail.com>
2024-02-17 19:36:51 +0530

    datacollection

--- datacollection.py
+++ datacollection.py
@@ -7,12 +7,29 @@ facedetect = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalf
 
 photoCount = 0
 
-name = input("Enter your name: ")
-PATH = 'images/' + name
+nameID = input("Enter your name: ")
+PATH = 'images/' + nameID
 
 if os.path.exists(PATH):
-    print("Your name already exist in the database.")
-    quit
+    print("Your name already exist in the database.");
+    exit() 
 else:
     os.makedirs(PATH)
-    print("path created")
+    
+
+while photoCount < 500:
+    rect, frame = video.read()
+    faces = facedetect.detectMultiScale(frame, 1.3, 5)
+    for x, y, w, h in faces:
+        photoCount += 1
+        name = 'images/' + nameID + '/' + str(photoCount) + '.jpg'
+        cv2.imwrite(name, frame[y:y+h, x:x+w])
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow("Camera", frame)
+    cv2.waitKey(1)
+
+video.release()
+cv2.destroyAllWindows()
+
+
+
